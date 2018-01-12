da620898440e0dbd6e1eb1c6c0639bf05dfb761b
Badger <mike@devbadger.com>
2018-01-12 12:06:34 -0700

    update bootstrap

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PWD=pwd
+
+MAX_RETRIES=3
+
+#build eureka
+cd /Users/z002ngh/Projects/devBadger/spring/eureka/ && ./gradlew clean build
