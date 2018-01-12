62220a794e2101bb478be6a6d1961e7bf5c1c760
Badger <mike@devbadger.com>
2018-01-12 15:32:46 -0700

    update build script

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -5,8 +5,14 @@ PWD=pwd
 
 MAX_RETRIES=3
 
+#build wiremock
+cd /Users/z002ngh/Projects/devBadger/spring/wiremock && docker build -t "com.devbadger/wiremock:1.0.0-SNAPSHOT" .
+
 #build eureka
 cd /Users/z002ngh/Projects/devBadger/spring/eureka/ && ./gradlew clean build image jacocoTestReport
 
 #build config
 cd /Users/z002ngh/Projects/devBadger/spring/config/ && ./gradlew clean build image jacocoTestReport
+
+#build security
+cd /Users/z002ngh/Projects/devBadger/spring/security/ && ./gradlew clean build image jacocoTestReport
--- docker/start-stack.sh
+++ docker/start-stack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Rebuild WireMock image
-cd $WORKDIR/Tetris/Docker/wiremock && docker build -t "consensus/flex-wiremock:1.0.0-SNAPSHOT" .
+cd /Users/z002ngh/Projects/devBadger/spring/wiremock && docker build -t "com.devbadger/wiremock:1.0.0-SNAPSHOT" .
 
 #Check if docker network exists if not create one
 [ ! "$(docker ps -a | grep dockercomposefiles_consensus-network)" ] && docker network create dockercomposefiles_consensus-network
