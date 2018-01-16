82cd62c49a3086233f9d841c2becc4a49b944446
Badger <mike@devbadger.com>
2018-01-15 19:08:52 -0700

    Service settings

---
+++ service/service-intg/src/main/groovy/com/devbadger/serviceapp/contracts/GatewayClientContractSpec.groovy
@@ -0,0 +1,7 @@
+package com.devbadger.serviceapp.contracts
+
+/**
+ * Created by Badger on 1/15/18.
+ */
+class GatewayClientContractSpec {
+}
---
+++ service/service-intg/src/main/groovy/com/devbadger/serviceapp/contracts/StaticServerList.groovy
@@ -0,0 +1,25 @@
+package com.devbadger.serviceapp.contracts
+
+import com.netflix.loadbalancer.Server
+import com.netflix.loadbalancer.ServerList
+
+/**
+ * Created by Badger on 11/20/17.
+ */
+class StaticServerList<T extends Server> implements ServerList<T>{
+    private final List<T> servers
+
+    StaticServerList(List<T> servers) {
+        this.servers = servers
+    }
+
+    @Override
+    List<T> getInitialListOfServers() {
+        servers
+    }
+
+    @Override
+    List<T> getUpdatedListOfServers() {
+        servers
+    }
+}
