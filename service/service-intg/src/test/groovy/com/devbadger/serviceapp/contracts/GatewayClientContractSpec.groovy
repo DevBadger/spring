2c51c93aac5f31825237be38e64f6c440f993f1c
Badger <mike@devbadger.com>
2018-01-16 07:30:04 -0700

    update service-intg

---
+++ service/service-intg/src/test/groovy/com/devbadger/serviceapp/contracts/GatewayClientContractSpec.groovy
@@ -0,0 +1,9 @@
+package com.devbadger.serviceapp.contracts
+
+import spock.lang.Specification
+
+/**
+ * Created by Badger on 1/15/18.
+ */
+class GatewayClientContractSpec extends Specification{
+}
