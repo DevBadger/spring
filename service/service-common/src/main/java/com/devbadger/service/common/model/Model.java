78dab570fd54fcfff536485c61a5dcc91f51f826
Badger <mike@devbadger.com>
2018-01-15 17:43:42 -0700

    bootstrap settings

---
+++ service/service-common/src/main/java/com/devbadger/service/common/model/Model.java
@@ -0,0 +1,24 @@
+package com.devbadger.service.common.model;
+
+import org.pojomatic.Pojomatic;
+
+/**
+ * Created by Badger on 1/2/18.
+ */
+public abstract class Model {
+    @Override
+    public String toString() {
+        return Pojomatic.toString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return Pojomatic.hashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return Pojomatic.equals(this, other);
+    }
+
+}
