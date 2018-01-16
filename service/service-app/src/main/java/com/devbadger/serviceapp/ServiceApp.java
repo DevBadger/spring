aa09f14c1f1f74778dea7780ae4db11e9dc10bbf
Badger <mike@devbadger.com>
2018-01-15 17:52:36 -0700

    Service settings

---
+++ service/service-app/src/main/java/com/devbadger/serviceapp/ServiceApp.java
@@ -0,0 +1,41 @@
+package com.devbadger.serviceapp;
+
+import com.devbadger.service.common.model.ServiceObjectMapper;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@EnableAutoConfiguration
+@Configuration
+@EnableEurekaClient
+@SpringBootApplication
+@ComponentScan({ "com.devbadger.service"})
+@EnableFeignClients
+@Import({ServiceConfig.class})
+public class ServiceApp extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServiceApp.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication sa = new SpringApplication(ServiceApp.class);
+		sa.setBannerMode(Banner.Mode.OFF);
+		sa.run(args);
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingMappingJackson2HttpMessageConverter() {
+		return new MappingJackson2HttpMessageConverter(new ServiceObjectMapper());
+	}
+}
---
+++ service/service-app/src/main/java/com/devbadger/serviceapp/ServiceConfig.java
@@ -0,0 +1,10 @@
+package com.devbadger.serviceapp;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Badger on 1/15/18.
+ */
+@Configuration
+public class ServiceConfig {
+}
