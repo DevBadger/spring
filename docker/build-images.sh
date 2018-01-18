fa29d245f1c518785f65c029bf7d792f466f4f00
DevBadger <mike@devbadger.com>
2018-01-18 09:24:14 -0700

    add config for gtep

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
-
+PROJDIR="/Users/michaelbadger/IdeaProjects/DevBadger/spring"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=pwd
 
 MAX_RETRIES=3
 
 #build wiremock
-cd /Users/z002ngh/Projects/devBadger/spring/wiremock && docker build -t "com.devbadger/wiremock:1.0.0-SNAPSHOT" .
+cd $PROJDIR/wiremock && docker build -t "com.devbadger/wiremock:1.0.0-SNAPSHOT" .
 
 #build eureka
-cd /Users/z002ngh/Projects/devBadger/spring/eureka/ && ./gradlew clean build image jacocoTestReport
+cd $PROJDIR/eureka/ && ./gradlew clean build image jacocoTestReport
 
 #build config
-cd /Users/z002ngh/Projects/devBadger/spring/config/ && ./gradlew clean build image jacocoTestReport
+cd $PROJDIR/config/ && ./gradlew clean build image jacocoTestReport
 
 #build zuul
-cd /Users/z002ngh/Projects/devBadger/spring/zuul/ && ./gradlew clean build image jacocoTestReport
-
-#build security
-cd /Users/z002ngh/Projects/devBadger/spring/security && ./gradlew clean build install image jacocoTestReport
+cd $PROJDIR/zuul/ && ./gradlew clean build image jacocoTestReport
 
 #build gateway
-cd /Users/z002ngh/Projects/devBadger/spring/gateway/gateway-app && ./gradlew generateContractTests
-cd /Users/z002ngh/Projects/devBadger/spring/gateway && ./gradlew clean build install image jacocoTestReport
-cd /Users/z002ngh/Projects/devBadger/spring/gateway/gateway-app && ./gradlew generateClientStubs publishToMavenLocal
+cd $PROJDIR/gateway/gateway-icndb-model && ./gradlew clean build install
+cd $PROJDIR/gateway/gateway-icndb-service && ./gradlew clean build install
+cd $PROJDIR/gateway/gateway-app && ./gradlew generateContractTests
+cd $PROJDIR/gateway && ./gradlew clean build install image jacocoTestReport
+cd $PROJDIR/gateway/gateway-app && ./gradlew generateClientStubs publishToMavenLocal
 
 #build service
-cd /Users/z002ngh/Projects/devBadger/spring/service
+cd $PROJDIR/service
 ./gradlew clean build -x test install publishToMavenLocal
 ./gradlew build image jacocoTestReport
